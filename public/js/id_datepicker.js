4195517a9a7c967cb7c4ca388e4783ffcf9968ef
nandou24 <nandou20@hotmail.com>
2024-09-11 11:00:03 -0500

    table phone 1.1

--- public/js/id_datepicker.js
+++ public/js/id_datepicker.js
@@ -13,7 +13,14 @@ function insertarFila(){
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
     cell1.innerHTML = "1";
     cell2.innerHTML = document.getElementById('telfPaciente').value;
     cell3.innerHTML = document.getElementById('telfDes').value;
+    cell4.innerHTML = '<td><input class="btn btn-danger" type="button" value="Eliminar" onclick="deleteRow(this)"></td>'
 }
+
+function deleteRow(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("phoneTable").deleteRow(i);
+  }
