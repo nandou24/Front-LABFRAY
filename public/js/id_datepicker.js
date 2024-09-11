203d7381b5208a6fcb599d1a763dfc0f4d9cdd4d
nandou24 <nandou20@hotmail.com>
2024-09-11 01:52:33 -0500

    tabla teléfono 1.0

--- public/js/id_datepicker.js
+++ public/js/id_datepicker.js
@@ -6,3 +6,14 @@ $(function () {
             todayHighlight: true
         })
 });
+
+function insertarFila(){
+    let phoneTable = document.getElementById('phoneTable');
+    let row = phoneTable.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    cell1.innerHTML = "1";
+    cell2.innerHTML = document.getElementById('telfPaciente').value;
+    cell3.innerHTML = document.getElementById('telfDes').value;
+}
