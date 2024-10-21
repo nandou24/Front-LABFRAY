5a211ca38f09d4489f182c6f65c1904ac84599fa
nandou24 <nandou20@hotmail.com>
2024-10-21 13:05:00 -0500

    array teléfono dentro del mismo form

--- public/js/id_datepicker.js
+++ public/js/id_datepicker.js
@@ -4,25 +4,8 @@ $(function () {
             language: 'es',
             autoclose: true,
             todayHighlight: true
+        }).on('changeDate',(e)=>{
+            this.calcularEdad(e.Date)
         })
 });
 
-/*
-function insertarFila(){
-    let phoneTable = document.getElementById('phoneTable');
-    let row = phoneTable.insertRow(-1);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    cell1.innerHTML = "1";
-    cell2.innerHTML = document.getElementById('telfPaciente').value;
-    cell3.innerHTML = document.getElementById('telfDes').value;
-    cell4.innerHTML = '<td><input class="btn btn-danger" type="button" value="Eliminar" onclick="deleteRow(this)"></td>'
-}
-
-function deleteRow(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-    document.getElementById("phoneTable").deleteRow(i);
-}
-*/
--- src/app/pages/registrar-paciente/registrar-paciente.component.ts
+++ src/app/pages/registrar-paciente/registrar-paciente.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IUser } from 'src/app/auth/models/auth.models';
 import { AuthService } from 'src/app/auth/services/auth.service';
@@ -19,31 +19,88 @@ export class RegistrarPacienteComponent implements OnInit{
     private _router: Router,
     private _authService: AuthService,
     private _datePickerService:  DatepickerService,
-  ) {
-    this.phoneData = [];
-  }
+  ) { }
 
   ngOnInit(): void {
     this._datePickerService.loadScript();
+
+    this.myForm.get('fechaNacimiento')?.valueChanges.subscribe(value => {
+      console.log('Fecha de nacimiento modificada:', value);
+      this.calcularEdad(value);  // Aquí puedes ejecutar la lógica que necesites
+    });
   }
  
   public myForm:FormGroup  = this._fb.group({
       name: ['', [Validators.required]],  
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      rol: ['',[Validators.required]]
+      rol: ['',[Validators.required]],
+      fechaNacimiento: ['', Validators.required],
+      phones: this._fb.array([])
   });
 
-  public phoneForm:FormGroup  = this._fb_phone.group({
-    phoneNumber: ['', [Validators.required, Validators.maxLength(11)]],  
-    descriptionPhone: ['', [Validators.required]],
-  });
 
-  public phoneData: any;
+  public newPhone = {
+    phoneNumber: '',        // Campo para el número de teléfono
+    descriptionPhone: ''    // Campo para la descripción del teléfono
+  };
+
+  public edad: number | null = null;
+
+  crearTelefono(): FormGroup{
+    return this._fb.group({
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      descriptionPhone: ['',[Validators.required]]
+    })
+  }
+
+  get phones(): FormArray{
+    return this.myForm.get('phones') as FormArray;
+  }
 
   addPhoneItem():void{
-    this.phoneData.push(this.phoneForm.value);
-    this.phoneForm.reset();
+    if (this.newPhone.phoneNumber && this.newPhone.descriptionPhone) {
+
+      const nuevoTelefonoFormGroup = this._fb.group({
+        phoneNumber: [this.newPhone.phoneNumber, [Validators.required, Validators.pattern('^[0-9]+$')]],
+        descriptionPhone: [this.newPhone.descriptionPhone, [Validators.required]]
+      });
+
+      this.phones.push(nuevoTelefonoFormGroup);
+
+      this.newPhone = {phoneNumber:'', descriptionPhone:''}
+
+    }
+  }
+
+  removeItem(index:number){
+    this.phones.removeAt(index)
+    console.log(this.phones)
+  }
+
+  calcularEdad(fechaNacimiento: any): void {
+    
+    console.log('Transformando fecha')
+    const [day, month, year] = fechaNacimiento.split('/');
+    const nuevaFecha = `${month}/${day}/${year}`;
+    
+    console.log('calculando')
+    //const fechaNacimiento = this.myForm.get('fechaNacimiento')?.value;  // Obtener el valor del FormControl
+    if (fechaNacimiento) {
+      const today = new Date();  // Fecha actual
+      const birthDate = new Date(nuevaFecha);  // Convertir a fecha
+      let age = today.getFullYear() - birthDate.getFullYear();  // Calcular la diferencia de años
+      const monthDiff = today.getMonth() - birthDate.getMonth();  // Verificar el mes
+
+      // Ajustar la edad si aún no ha cumplido años este año
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      this.edad = age;  // Actualizar la variable de edad
+    } else {
+      this.edad = null;  // Si no hay fecha, limpiar la edad
+    }
   }
 
   userRegister(){
